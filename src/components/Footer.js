469e5db8147deb63e90a4b24a66d5a621878d80d
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2023-09-20 22:25:43 +0530

    Added Exercise Detail Page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from "../assets/images/Logo.png";
 
-const Footer = () => {
-  return <div>Footer</div>;
-};
+const Footer = () => (
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Stack
+      direction="row"
+      gap="10px"
+      sx={{ alignItems: "center", justifyContent: "center" }}
+      flexWrap="wrap"
+      p="20px"
+    >
+      <img
+        src={Logo}
+        alt="logo"
+        style={{
+          width: "48px",
+          height: "48px",
+          margin: "0 20px",
+        }}
+      />
+      <Typography
+        variant="h4"
+        style={{
+          fontWeight: "bold",
+          alignItems: "center",
+          color: "#ff2625",
+          textAlign: "center",
+          justifyContent: "center",
+        }}
+      >
+        Momentum
+      </Typography>
+    </Stack>
+  </Box>
+);
 
 export default Footer;
